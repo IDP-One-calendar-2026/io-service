4ff584e165bb890ad24ef80d0331a5907f914a31
Manuel <manuelraileanu@gmail.com>
2026-04-27 22:02:05 +0300

    debug logs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 const url =
   process.env.DATABASE_URL_IO_SERVICE ??
   `postgresql://${process.env.DB_USER_IO_SERVICE!}:${process.env.DB_PASSWORD_IO_SERVICE!}@${process.env.DB_HOST_IO_SERVICE!}:${process.env.DB_PORT_IO_SERVICE!}/${process.env.DB_NAME_IO_SERVICE!}`;
-
+console.log(url)
 export default defineConfig({
   dbCredentials:{
     url
